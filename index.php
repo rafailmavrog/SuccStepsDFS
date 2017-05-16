6a21d7cd63980cf9ea78efdd4d2c9472f450d21f
Rafail Mavrogordatos <rafailmavrog2017@gmail.com>
2017-05-16 17:13:44 +0300

    Completed display_admin, connect, buildDB functions on index.php

--- index.php
+++ index.php
@@ -1,1 +1,58 @@
-<?php?>
+<?php
+
+class SuccStepsDFS{
+   var $host;
+   var $username;
+   var $password;
+   var $table;
+
+   public function display_public(){
+
+   }
+   
+   public function display_admin(){
+    return <<<ADMIN_FORM
+
+    <form action="{$_SERVER['PHP_SELF']}" method="post">
+      <label for="title">Title:</label>
+      <input name="title" id="title" type="text" maxlength="150" />
+      <label for="bodytext">Body Text:</label>
+      <textarea name="bodytext" id="bodytext"></textarea>
+      <input type="submit" value="Create This Entry!" />
+    </form>
+
+ADMIN_FORM;
+}
+
+
+
+   }
+
+   public function write(){
+
+   }
+   
+   public function connect(){
+       mysql_connect($this->host,$this->username,$this->password) or die("Could not connect. " . mysql_error());
+       mysql_select_db($this->table) or die("Could not select database" . mysql_error());
+
+       return $this->buildDB();
+
+   }
+
+   private function buildDB(){
+        $sql = <<<MySQL_QUERY
+            CREATE TABLE IF NOT EXISTS testDB (
+              title VARCHAR(150),
+              bodytext TEXT,
+              created VARCHAR(100)
+              )
+              MySQL_QUERY;
+
+              return mysql_query($sql);
+   } 
+
+}
+
+
+?>
