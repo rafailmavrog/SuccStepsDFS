9b55356fc2780d992b55b00b681481eb6f9f679b
Rafail Mavrogordatos <rafailmavrog2017@gmail.com>
2017-05-17 12:47:39 +0300

    Changed whole index.php

--- index.php
+++ index.php
@@ -1,64 +1,30 @@
-<?php
+?php
+include('login.php'); // Includes Login Script
 
-class SuccStepsDFS{
-   var $host;
-   var $username;
-   var $password;
-   var $table;
-
-   public function display_public(){
-
-   }
-   
-   public function display_admin(){
-    return <<<ADMIN_FORM
-
-    <form action="{$_SERVER['PHP_SELF']}" method="post">
-      <label for="title">Title:</label>
-      <input name="title" id="title" type="text" maxlength="150" />
-      <label for="bodytext">Body Text:</label>
-      <textarea name="bodytext" id="bodytext"></textarea>
-      <input type="submit" value="Create This Entry!" />
-    </form>
-
-ADMIN_FORM;
-
-   private function create_customer(){
-       
-
-
-   }
+if(isset($_SESSION['login_user'])){
+header("location: profile.php");
 }
-
-
-
-   }
-
-   public function write(){
-
-   }
-   
-   public function connect(){
-       mysql_connect($this->host,$this->username,$this->password) or die("Could not connect. " . mysql_error());
-       mysql_select_db($this->table) or die("Could not select database" . mysql_error());
-
-       return $this->buildDB();
-
-   }
-
-   private function buildDB(){
-        $sql = <<<MySQL_QUERY
-            CREATE TABLE IF NOT EXISTS testDB (
-              title VARCHAR(150),
-              bodytext TEXT,
-              created VARCHAR(100)
-              )
-              MySQL_QUERY;
-
-              return mysql_query($sql);
-   } 
-
-}
-
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>SuccSteps Salon Management</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="main">
+<h1>Administrator Login</h1>
+<div id="login">
+<h2>Login Form</h2>
+<form action="" method="post">
+<label>UserName :</label>
+<input id="name" name="username" placeholder="username" type="text">
+<label>Password :</label>
+<input id="password" name="password" placeholder="**********" type="password">
+<input name="submit" type="submit" value=" Login ">
+<span><?php echo $error; ?></span>
+</form>
+</div>
+</div>
+</body>
+</html>
